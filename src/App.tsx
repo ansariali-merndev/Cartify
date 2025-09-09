ba28576e893b3654493113ba920ecc9c88a7d5fb
ansariali <=>
2025-09-09 22:00:09 +0530

    fix deploy problem

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,36 @@ import { UserProvider } from "./Context/UserProvider";
 import { Store } from "./components/layout/Store";
 import { Product } from "./components/layout/Product";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/store",
+          element: <Store />,
+        },
+        {
+          path: "/store/:id",
+          element: <Product />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/store",
-        element: <Store />,
-      },
-      {
-        path: "/store/:id",
-        element: <Product />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-    ],
-  },
-]);
+    basename: "/Cartify/",
+  }
+);
 
 function App() {
   return (
